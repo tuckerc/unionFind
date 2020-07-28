d5511992b1044348541ffb4e62237c4574ace69a
Chase Tucker <tuck8106@gmail.com>
2020-07-27 17:50:39 -0700

    quick find implemented and tested

--- src/main/java/unionfind/QuickFind.java
+++ src/main/java/unionfind/QuickFind.java
@@ -1,4 +1,34 @@
 package unionfind;
 
 public class QuickFind {
+    // instance vars
+    int[] nodes;
+
+    // constructors
+    public QuickFind(int size) {
+        nodes = new int[size];
+        for(int i = 0; i < nodes.length; i++) {
+            nodes[i] = i;
+        }
+    }
+
+    // accessor methods
+    public int[] getNodes() {
+        return nodes;
+    }
+
+    // business methods
+    public void union(int firstNode, int secondNode) {
+        int oldVal = nodes[secondNode];
+        int newVal = nodes[firstNode];
+        for(int i = 0; i < nodes.length; i++) {
+            if(nodes[i] == oldVal) {
+                nodes[i] = newVal;
+            }
+        }
+    }
+
+    public boolean find(int firstNode, int secondNode) {
+        return nodes[firstNode] == nodes[secondNode];
+    }
 }
--- src/test/java/unionfind/QuickFindTest.java
+++ src/test/java/unionfind/QuickFindTest.java
@@ -1,11 +1,45 @@
 package unionfind;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class QuickFindTest {
 
+  QuickFind qf;
+
   @Before
-  public void setUp() throws Exception {}
+  public void setUp() throws Exception {
+    qf = new QuickFind(10);
+    qf.union(1, 2);
+    qf.union(3, 4);
+    qf.union(5, 6);
+    qf.union(7, 8);
+    qf.union(3, 8);
+    qf.union(0, 9);
+    qf.union(9, 5);
+  }
+
+  @Test
+  public void unionTest() {
+    assertArrayEquals(new int[]{0, 1, 1, 3, 3, 0, 0, 3, 3, 0}, qf.getNodes());
+  }
+
+  @Test
+  public void trueFindTest() {
+    assertTrue(qf.find(1, 2));
+    assertTrue(qf.find(3, 4));
+    assertTrue(qf.find(0, 5));
+    assertTrue(qf.find(8, 3));
+    assertTrue(qf.find(9, 0));
+  }
+
+  @Test
+  public void falseFindTest() {
+    assertFalse(qf.find(0, 1));
+    assertFalse(qf.find(3, 5));
+    assertFalse(qf.find(7, 6));
+    assertFalse(qf.find(9, 1));
+  }
 }
