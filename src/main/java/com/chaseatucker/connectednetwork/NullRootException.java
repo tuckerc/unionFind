14bbe44342a51bbd1b1c176d6dc105be37d56d43
Chase Tucker <tuck8106@gmail.com>
2020-07-30 16:49:42 -0700

    findMax and findMin methods implemented and tested

--- src/main/java/com/chaseatucker/connectednetwork/NullRootException.java
+++
@@ -1,7 +0,0 @@
-package com.chaseatucker.connectednetwork;
-
-public class NullRootException extends Exception {
-    public NullRootException(String message) {
-        super(message);
-    }
-}
--- src/test/java/unionfind/WeightedQuickUnionTest.java
+++ src/test/java/unionfind/WeightedQuickUnionTest.java
@@ -28,19 +28,48 @@ public class WeightedQuickUnionTest {
   }
 
   @Test
-  public void trueFindTest() {
-    assertTrue(qu.find(1, 2));
-    assertTrue(qu.find(3, 4));
-    assertTrue(qu.find(0, 5));
-    assertTrue(qu.find(8, 3));
-    assertTrue(qu.find(9, 0));
+  public void trueConnectionsTest() {
+    assertTrue(qu.connected(1, 2));
+    assertTrue(qu.connected(3, 4));
+    assertTrue(qu.connected(0, 5));
+    assertTrue(qu.connected(8, 3));
+    assertTrue(qu.connected(9, 0));
   }
 
   @Test
-  public void falseFindTest() {
-    assertFalse(qu.find(0, 1));
-    assertFalse(qu.find(3, 5));
-    assertFalse(qu.find(7, 6));
-    assertFalse(qu.find(9, 1));
+  public void falseConnectionsTest() {
+    assertFalse(qu.connected(0, 1));
+    assertFalse(qu.connected(3, 5));
+    assertFalse(qu.connected(7, 6));
+    assertFalse(qu.connected(9, 1));
+  }
+
+  @Test
+  public void findMaxHappyTest() {
+    assertEquals(9, qu.findMax(0));
+    assertEquals(9, qu.findMax(6));
+    assertEquals(8, qu.findMax(3));
+  }
+
+  @Test
+  public void findMaxFailTest() {
+    assertNotEquals(6, qu.findMax(6));
+    assertNotEquals(4, qu.findMax(2));
+    assertNotEquals(3, qu.findMax(3));
+  }
+
+  @Test
+  public void findMinHappyTest() {
+    assertEquals(0, qu.findMin(0));
+    assertEquals(1, qu.findMin(3));
+    assertEquals(0, qu.findMin(6));
+    assertEquals(1, qu.findMin(2));
+  }
+
+  @Test
+  public void findMinFailTest() {
+    assertNotEquals(6, qu.findMin(6));
+    assertNotEquals(4, qu.findMin(2));
+    assertNotEquals(3, qu.findMin(3));
   }
 }
